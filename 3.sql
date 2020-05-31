c10dd994c598b309ec4a466531cc326ec48dca4e
AlbertLatypov <48538192+AlbertLatypov@users.noreply.github.com>
2020-05-31 18:33:15 +0300

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,4 +1,10 @@
-select city.name, income.ddate, income.ndoc, goods_groups.name, goods.name, (goods.length*goods.width*goods.height)*incgoods.volume
+
+select city.name, 
+income.ddate, 
+income.ndoc, 
+goods_groups.name, 
+goods.name, 
+(goods.length*goods.width*goods.height)*incgoods.volume
 
 from clients join income on clients.id = income.client
 join city on clients.city=city.id
@@ -6,13 +12,22 @@ join incgoods on income.id=incgoods.id
 join goods on incgoods.goods=goods.id
 join goods_groups on goods.id=goods_groups.parent
 
-where (goods.length*goods.width*goods.height)*incgoods.volume > 10 and date_part('month',income.ddate) between 4 and 6 and date_part('year', income.ddate) = 2020;
+where (goods.length*goods.width*goods.height)*incgoods.volume > 10 
+and date_part('month',income.ddate) between 4 and 6 
+and date_part('year', income.ddate) = 2020;
 
 
 
 
 
-select clients.address, recept.ddate, recept.ndoc, storages.name, goods.name, (goods.weight*recgoods.volume), (recgoods.volume * recgoods.price)
+select 
+clients.address, 
+recept.ddate, 
+recept.ndoc, 
+storages.name, 
+goods.name, 
+(goods.weight*recgoods.volume), 
+(recgoods.volume * recgoods.price)
 
 from recept
 join clients on recept.client=clients.id
@@ -22,7 +37,10 @@ join goods on recgoods.goods=goods.id
 join city on clients.city=city.id
 join region on city.region=region.id
 
-where region.name='Moscow' and date_part('year', recept.ddate)=2020 and date_part('month', recept.ddate)=2 and recgoods.id in
+where region.name='Moscow' 
+and date_part('year', recept.ddate)=2020 
+and date_part('month', recept.ddate)=2 
+and recgoods.id in
                (select recgoods.id
                from recept 
                 join recgoods on recept.id=recgoods.id
