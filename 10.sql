aa0a206f2718a7a5128558c49a43b3c2d812280c
AlbertLatypov <48538192+AlbertLatypov@users.noreply.github.com>
2020-06-06 05:38:40 +0300

    Update 10.sql

--- 10.sql
+++ 10.sql
@@ -1,13 +1,12 @@
 WITH RECURSIVE r AS (
-   SELECT id, par_id
-   FROM drevo
+   SELECT id, parent
+   FROM goods_groups
 
-   UNION
+   union
+   
+   select goods_groups.id, r.parent 
+	from goods_groups join r on goods_groups.parent = r.id
+	where r.parent is not null
 
-   SELECT drevo.id, drevo.par_id
-   FROM drevo
-      JOIN r
-          ON drevo.par_id = r.id
 )
-
 SELECT * FROM r;
