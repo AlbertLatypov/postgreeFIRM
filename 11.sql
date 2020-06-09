311ab2ece21a53cf1641eb8ffbadc524e488dfd2
AlbertLatypov <48538192+AlbertLatypov@users.noreply.github.com>
2020-06-09 08:06:57 +0300

    Update 11.sql

--- 11.sql
+++ 11.sql
@@ -11,7 +11,29 @@ WITH RECURSIVE r AS (
 SELECT * FROM r
 
 
+WITH RECURSIVE r AS (
+   SELECT id, par_id 
+   FROM drevo
 
+   union
+   
+   select drevo.id, r.par_id
+	from drevo join r on drevo.par_id = r.id
+	where r.par_id is not null
+)
+
+
+select 
+drevo_goods.good_id,
+drevo_goods.good_gr
+from drevo_goods join drevo on drevo_goods.good_gr = drevo.id
+
+union
+
+select
+drevo_goods.good_id,
+r.par_id
+from drevo_goods join r on drevo_goods.good_gr = r.id
 
 
 
